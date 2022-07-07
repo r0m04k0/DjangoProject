596f157acd487fbea462953110ce8430c47ce9db
romo4ko <romo4ko.mail@gmail.com>
2022-07-07 23:24:20 +0300

    Fix some problems

--- DjangoProject/asgi.py
+++ DjangoProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for name project.
+ASGI config for DjangoProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoProject.settings')
 
 application = get_asgi_application()
